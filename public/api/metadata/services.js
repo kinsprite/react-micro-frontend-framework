ac24622d31bc11f37ba945d3f9313f0daa9eaa62
kinsprite <qxqs11@126.com>
2020-06-19 08:48:05 +0800

    add metadata services template

---
+++ public/api/metadata/services.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+"use strict";
+
+(function(callback) {
+  var services = {
+    // [serviceName: string]: {}
+    appExample: {
+      name: 'appExample',
+      dependencies: ['echart', 'app1'],
+      routes: ['/appExample'],
+      styles: ['/app-example-f00322/app-example-a200b003.css'],
+      entries: ['/app-example-f00322/app-example-b889c22.js'],
+    }
+  };
+
+  if (callback) callback(services);
+})(rmsMetadataServicesCallback);
