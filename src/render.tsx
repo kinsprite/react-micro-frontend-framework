650322ff9626a4f33a84830b4de1e6bc6ce9c49a
kinsprite <qxqs11@126.com>
2020-06-22 18:05:25 +0800

    add redux Provider

--- src/render.tsx
+++ src/render.tsx
@@ -9,43 +9,55 @@ import {
   Link,
 } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
 import App from './App';
 import { getRegister, AsyncApp } from './micro';
 import { RedirectToDefaultRoute } from './util';
+import { getStore } from './store';
 
-export default function render(element: Element) : void {
+function RouterBase() {
   const apps = getRegister().getAppsByRoutes();
   const redirectDefault = RedirectToDefaultRoute();
 
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/app-example">App Example</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {apps.map((pair) => (
+            <Route path={pair.route} key={pair.route}>
+              <AsyncApp routePath={pair.route} />
+            </Route>
+          ))}
+          <Route path="/home">
+            <App />
+          </Route>
+          <Route exact path="/">
+            { redirectDefault }
+          </Route>
+          )
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default function render(element: Element) : void {
+  const store = getStore();
+
   ReactDOM.render(
     <React.StrictMode>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/app-example">App Example</Link>
-            </li>
-          </ul>
-
-          <Switch>
-            {apps.map((pair) => (
-              <Route path={pair.route} key={pair.route}>
-                <AsyncApp routePath={pair.route} />
-              </Route>
-            ))}
-            <Route path="/home">
-              <App />
-            </Route>
-            <Route exact path="/">
-              { redirectDefault }
-            </Route>
-            )
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <RouterBase />
+      </Provider>
     </React.StrictMode>,
     element,
   );
