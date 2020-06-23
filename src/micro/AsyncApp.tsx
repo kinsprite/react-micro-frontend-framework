13a30cfa9d41ddde524db57451949a70fbdf9d19
kinsprite <qxqs11@126.com>
2020-06-23 09:49:53 +0800

    fix AsyncApp run many times

--- src/micro/AsyncApp.tsx
+++ src/micro/AsyncApp.tsx
@@ -11,43 +11,60 @@ interface AsyncAppProps {
   [key: string]: any;
 }
 
+enum LoadedState {
+  Init,
+  OK,
+  Failed,
+}
+
 function AsyncApp(props : AsyncAppProps) : React.ReactElement {
   const { appId, routePath, redirectOnFail } = props;
   const register = getRegister();
   const app = register.getApp(appId);
 
-  // can't use useState() for React.createElement
-  let component : any = app && app.component;
-  const redirectElement = redirectOnFail ? <Redirect to={redirectOnFail} /> : RedirectToDefaultRoute(routePath);
+  const isAppLoaded = app && register.isAppLoaded(app.id);
 
-  const [loaded, setLoaded] = useState(app && register.isAppLoaded(app.id));
-  const [redirect, setRedirect] = useState(false);
+  const [once] = useState(1);
+  const [result, setResult] = useState(
+    isAppLoaded ? {
+      loaded: LoadedState.OK,
+      component: app && app.component, // can't use React.FC in useState() for React.createElement
+    } : {
+      loaded: LoadedState.Init,
+      component: null,
+    },
+  );
 
   useEffect(() => {
     let isMounted = true;
 
-    if (app && !loaded) {
+    if (app && result.loaded === LoadedState.Init) {
       register.loadApp(app.id).then(() => {
         if (isMounted) {
-          component = app.component;
-          setLoaded(true);
+          setResult({
+            loaded: LoadedState.OK,
+            component: app.component,
+          });
         }
       }).catch(() => {
         if (isMounted) {
-          setLoaded(false);
-          setRedirect(true);
+          setResult({
+            loaded: LoadedState.Failed,
+            component: null,
+          });
         }
       });
     }
 
     return () => { isMounted = false; };
-  }, [loaded]);
+  }, [once]);
 
-  if (redirect) {
-    return redirectElement;
+  if (result.loaded === LoadedState.Failed) {
+    return redirectOnFail ? <Redirect to={redirectOnFail} /> : RedirectToDefaultRoute(routePath);
   }
 
-  return (loaded && component) ? React.createElement(component, props) : <></>;
+  return (result.loaded === LoadedState.OK && result.component)
+    ? React.createElement(result.component as any, props) : <></>;
 }
 
 export default AsyncApp;
